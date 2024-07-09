53a090e568ba736faeabfbcd8eb88d6c4e819423
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-07-09 18:31:19 +0000

    added modal to products.js

--- src/app/components/Products.js
+++ src/app/components/Products.js
@@ -1,9 +1,24 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CoffeeCards from "./CoffeeCards";
+import Cart from './Cart'
+import { CartContext } from "./ShoppingCart";
+import Image from "next/image";
 
 export default function Product() {
     const [product, setProduct] = useState([]);
+    const [showModal, setShowModal] = useState(false)
+    const { cartItems, addToCart } = useContext(CartContext)
+
+    const toggle = () => {
+      setShowModal(!showModal)
+    }
+
+    {!showModal && <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
+      onClick={toggle}
+    >Cart ({cartItems.length})</button>}
+
+    
     useEffect(()=> {
         async function wakeupServer(){
           const response = await axios.get('https://coffee-shop-backend-d4a9.onrender.com/product')
@@ -15,12 +30,37 @@ export default function Product() {
         
           }, []) 
    return (
-    <>
-    <div className="coffee-products">
-    <h2>Our Coffee Products</h2>
-      {product.map((product) => (
-        <CoffeeCards key={product.id} product={product} />
-      ))}
-    </div>
-    </>)     
+ <CoffeeCards className='flex flex-col justify-center bg-gray-100'>
+      <div className='flex justify-between items-center px-20 py-5'>
+        <h1 className='text-2xl uppercase font-bold mt-10 text-center mb-10'>Shop</h1>
+        {!showModal && <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
+          onClick={toggle}
+        >Cart ({cartItems.length})</button>}
+      </div>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10'>
+        {
+          product.map(product => (
+            <CoffeeCards key={product.id} className='bg-white shadow-md rounded-lg px-10 py-10'>
+              <Image src={product.thumbnail} alt={product.title} className='rounded-md h-48' />
+              <div className='mt-4'>
+                <h1 className='text-lg uppercase font-bold'>{product.title}</h1>
+                <p className='mt-2 text-gray-600 text-sm'>{product.description.slice(0, 40)}...</p>
+                <p className='mt-2 text-gray-600'>${product.price}</p>
+              </div>
+              <div className='mt-6 flex justify-between items-center'>
+                <button className='px-4 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
+                  onClick={() => {
+                    addToCart(product)
+                  }
+                  }
+                >Add to cart</button>
+              </div>
+            </CoffeeCards>
+          ))
+        }
+      </div>
+      <Cart showModal={showModal} toggle={toggle} />
+    </CoffeeCards>
+    )     
 }
+
