a6f45ecda127f1bc4a5970c6ea39d136579d95c1
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-07-11 02:31:17 +0000

    Created hero file

---
+++ src/app/components/Hero.js
@@ -0,0 +1,18 @@
+
+
+export default function Hero() {
+    
+    return (
+        <div className="relative">
+        <div className="home"></div>
+         <div className="p-5 text-white p-4 md:p-8 absolute start-0 right-0 top-1/3 end-0">
+<div className= "w-full md:w-auto">
+    <h1 className='text-lg md:text-4xl'>Discover the Perfect Cup: Explore Our Premium Coffee Products</h1>
+    <a className='btn btn-secondary me-2' href="#shop">Brew Happiness: Shop Our Coffee Products Now</a>
+</div> 
+</div>
+        </div>
+       
+       
+    )
+}
