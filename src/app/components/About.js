de0ad936812c6664351809a4b43c4545bbcc2896
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-07-12 00:36:36 +0000

    created about.js

---
+++ src/app/components/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section className="bg-gray-200 py-12 mt-12 mb-12" id='about'>
+    <div className="container mx-auto px-4">
+      <h2 className="text-3xl font-bold text-center mb-6">About Sunrise Sips</h2>
+      <p className="text-lg text-center mb-8">
+        Wake up to a world of flavor.
+      </p>
+      <p className="text-gray-600 text-center mb-4">
+        Sunrise Sips is more than just a coffee shop; it's your daily dose of inspiration. We're passionate about crafting exceptional coffee experiences that ignite your senses and fuel your day. From rich, aromatic blends to smooth, velvety cold brews, we've perfected the art of coffee to bring you the ultimate taste sensation.
+      </p>
+      <p className="text-gray-600 text-center mb-4">
+        Our carefully curated selection of coffee products ensures there's something for every palate. Whether you're a seasoned coffee connoisseur or just starting your caffeine journey, we invite you to explore our range and discover your new favorite brew.
+      </p>
+      <p className="text-gray-600 text-center">
+        Join us in embracing the sunrise and savoring life's simple pleasures, one sip at a time.
+      </p>
+    </div>
+  </section>
+  )
+}
