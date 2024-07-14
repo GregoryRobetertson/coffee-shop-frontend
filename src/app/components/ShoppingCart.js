afd9971a2131e5131e2121d4eec1b330e8aa1844
Gregory Robertson <147750592+GregoryRobetertson@users.noreply.github.com>
2024-07-14 20:29:30 +0000

    Refactor CartProvider to initialize cartItems from localStorage using useEffect
    Moved the logic to initialize cartItems from localStorage.getItem into a useEffect hook with an empty dependency array to ensure it runs only once on component mount. This change ensures that cartItems are properly initialized from localStorage when the CartProvider component mounts, improving data consistency and separation of concerns.
    in ShoppingCart.js

--- src/app/components/ShoppingCart.js
+++ src/app/components/ShoppingCart.js
@@ -1,12 +1,21 @@
 'use client';
 
-import { createContext, useState, useEffect } from 'react';
+
+import React, { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [showModal, setShowModal] = useState(false);
-  const [cartItems, setCartItems] = useState(localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []);
+  const [cartItems, setCartItems] = useState([]);
+
+  // Effect to initialize cartItems from localStorage
+  useEffect(() => {
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
+  }, []);
 
   const addToCart = (item) => {
     const isItemInCart = cartItems.find((cartItem) => cartItem._id === item._id);
@@ -48,16 +57,10 @@ export const CartProvider = ({ children }) => {
     return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
   };
 
+  // Effect to update localStorage when cartItems change
   useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }, [cartItems]);
-
-  useEffect(() => {
-    const storedCartItems = localStorage.getItem("cartItems");
-    if (storedCartItems) {
-      setCartItems(JSON.parse(storedCartItems));
-    }
-  }, []);
 
   return (
     <CartContext.Provider
